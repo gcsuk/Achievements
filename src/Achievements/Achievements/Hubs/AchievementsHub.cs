098acc9fe4d95dd92df528f2cf0d01d3fd263682
Rob King <rob@gcsuk.co.uk>
2018-09-26 19:02:08 +0100

    Removed notify button on demo client
    Removed some legacy test code

--- src/Achievements/Achievements/Hubs/AchievementsHub.cs
+++ src/Achievements/Achievements/Hubs/AchievementsHub.cs
@@ -54,35 +54,5 @@ namespace Achievements.Hubs
 
             return parsed["userId"];
         }
-
-        /// <summary>
-        ///  This is a bit shit but you can sit and monitor the database for new achievements and just tell the client if one appears
-        /// </summary>
-        [Obsolete("This was added while I was messing about. I left it in just to show a way of doing it without tying SignalR to Service Bus at all.")]
-        public async Task MonitorAchievements(string userId)
-        {
-            while (true) // If you call Stop on the connection while this is running you will get an error
-            {
-                var newAchievements = await _repository.GetNewForUserId(userId);
-
-                foreach (var achievement in newAchievements)
-                {
-                    var response = new AchievementUnlockedResponse
-                    {
-                        Category = achievement.Achievement.Category.Name,
-                        Name = achievement.Achievement.Name,
-                        Details = achievement.Achievement.Details
-                    };
-
-                    await _repository.SetNotNew(userId, achievement.Achievement.Id);
-
-                    foreach (var connectionId in Connections.GetConnections(userId))
-                        await Clients.Client(connectionId).SendAsync("Unlocked", JsonConvert.SerializeObject(response));
-                }
-
-                Thread.Sleep(60000); // Arbitrary polling period
-            }
-        }
-
     }
 }
--- src/Achievements/Achievements/wwwroot/JavaScript.js
+++ src/Achievements/Achievements/wwwroot/JavaScript.js
@@ -14,22 +14,15 @@ const start = async () => {
     await connection.start().catch(err => console.error(err.toString()));
     document.getElementById('start').disabled = true;
     document.getElementById('stop').disabled = false;
-    document.getElementById('notify').disabled = false;
 };
 
 const stop = async () => {
     await connection.stop().catch(err => console.error(err.toString()));
     document.getElementById('start').disabled = false;
     document.getElementById('stop').disabled = true;
-    document.getElementById('notify').disabled = true;
-};
-
-const notify = async () => {
-    connection.invoke('MonitorAchievements', '1');
 };
 
 document.getElementById('start').addEventListener('click', start);
 document.getElementById('stop').addEventListener('click', stop);
-document.getElementById('notify').addEventListener('click', notify);
 
 setupConnection();
