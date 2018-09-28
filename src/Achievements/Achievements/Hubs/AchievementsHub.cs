eb4b2d70d65a9baca9165f3d613364c695f7984e
David Fowler <davidfowl@gmail.com>
2018-09-27 23:31:46 -0700

    Added IUserIdProvider impl and cleaned up EventSender

--- src/Achievements/Achievements/Hubs/AchievementsHub.cs
+++ src/Achievements/Achievements/Hubs/AchievementsHub.cs
@@ -14,45 +14,6 @@ namespace Achievements.Hubs
     /// </summary>
     public class AchievementsHub : Hub
     {
-        private readonly IUserAchievementsRepository<string> _repository;
 
-        public static ConnectionMapping<string> Connections { get; } = new ConnectionMapping<string>();
-
-        public AchievementsHub(IUserAchievementsRepository<string> repository)
-        {
-            _repository = repository;
-        }
-
-        /// <summary>
-        /// Adds the user to the connection map
-        /// </summary>
-        public override async Task OnConnectedAsync()
-        {
-            Connections.Add(GetUserIdFromQueryString(), Context.ConnectionId);
-
-            await base.OnConnectedAsync();
-        }
-
-        /// <summary>
-        /// Removes the user from the connection map
-        /// </summary>
-        public override async Task OnDisconnectedAsync(Exception exception)
-        {
-            Connections.Remove(GetUserIdFromQueryString(), Context.ConnectionId);
-
-            await base.OnDisconnectedAsync(exception);
-        }
-
-        private string GetUserIdFromQueryString()
-        {
-            var queryString = Context.GetHttpContext().Request.QueryString;
-
-            if (queryString == null || !queryString.Value.Contains("userId"))
-                return null;
-
-            var parsed = HttpUtility.ParseQueryString(queryString.Value);
-
-            return parsed["userId"];
-        }
     }
 }
---
+++ src/Achievements/Achievements/Hubs/QueryStringUserIdProvider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Achievements.Hubs
+{
+	public class QueryStringUserIdProvider : IUserIdProvider
+    {
+        public string GetUserId(HubConnectionContext connection)
+        {
+            return connection.GetHttpContext().Request.Query["userId"];
+        }
+    }
+}
